7a62e133bf34988ab76ec6a229d43d7a749e2e79
chuonghoang99 <43929666+chuonghoang99@users.noreply.github.com>
2020-12-17 15:06:19 +0700

    update reponsive

--- Fontend/Script/scripts.js
+++ Fontend/Script/scripts.js
@@ -13,4 +13,23 @@ for (i = 0; i < dropdown.length; i++) {
     });
 }
 
-var staffBtn = document.getElementById("staff");
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+// function openNav() {
+//     console.log("aaaa");
+//     var leftMenu = document.getElementById("menu-left");
+//     console.log(leftMenu.style.display);
+//     if (leftMenu.style.display === "") {
+//         console.log("none");
+//         leftMenu.classList.add("open-menu-left");
+//         console.log(leftMenu.style.display);
+//     } else {
+//         leftMenu.classList.remove("open-menu-left");
+//     }
+// }
